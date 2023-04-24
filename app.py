0af0750b9fdff2555b49c4b11afd80a459fa7331
a1 <jennykim6890@naver.com>
2023-04-24 22:43:29 +0900

    feat : 나만의 버킷리스트 만들기

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+
+
 from pymongo import MongoClient
-import certifi
-
-ca = certifi.where();
-
-client = MongoClient('mongodb+srv://haroe:NaMQOAaOxZuDcXgt@cluster0.jbeqc4x.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
+client = MongoClient('localhost', 27017)
 db = client.dbsparta
+
+
 
 @app.route('/')
 def home():
